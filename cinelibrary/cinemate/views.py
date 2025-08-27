a526a7730b76eeaa9601ac6402c14d3b5f783bc0
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-27 18:30:51 +0200

    creating views

--- cinelibrary/cinemate/views.py
+++ cinelibrary/cinemate/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics
+from .models import Movie, Review
+from .serializers import MovieSerializer, ReviewSerializer, UserSerializer, RegisterSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permissions_classes = [permissions.IsAuthenticated]
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permissions_classes = [permissions.IsAuthenticated]
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
